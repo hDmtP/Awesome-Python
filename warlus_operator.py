57ef51ac50b566caa0f17c2edd787b43aaca7386
Pritam Dhara <65482473+hDmtP@users.noreply.github.com>
2020-11-17 23:52:08 +0530

    Add files via upload

---
+++ warlus_operator.py
@@ -0,0 +1,12 @@
+'''
+a = [1, 2, 3, 4] 
+if (n := len(a)) > 3: 
+	print(f"List is too long ({n} elements, expected <= 3)") 
+'''
+'''
+a = [1, 2, 3, 4] 
+n=len(a)
+if (len(a)) > 3: 
+	print(f"List is too long ({n} elements, expected <= 3)") 
+'''
+# Benefits of := rather than =
